9be0beda8e4240447e380c3316dd5f979c1bbe75
th-ofc <th.hodz@icloud.com>
2022-01-30 22:16:03 -0300

    verify pass in db

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,19 +5,31 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId = ""
+        let exists = true
 
-        for(var i = 0; i < 6; i++){
+        while(exists){
+            // cria um id aleatorio
+            for(var i = 0; i < 6; i++){
 
-            roomId += Math.floor(Math.random() * 10).toString()
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+            
+            // verifica se ja existe o id
+            const roomsExistId = await db.all(`SELECT id FROM rooms WHERE id = ${parseInt(roomId)}`)
+
+            if(!roomsExistId[0]){
+                //insere a sala no db
+                await db.run(`INSERT INTO rooms (
+                    id, 
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+                
+                exists = false
+            }   
         }
-
-        await db.run(`INSERT INTO rooms (
-            id, 
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close()
         
